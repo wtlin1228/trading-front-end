a9e015fb03f9ec2339cfd106de4b1d4df3b1749f
Cory Lee <cory8249@gmail.com>
2017-09-04 00:10:32 +0800

    better display

--- src/components/CurrentDealProductDisplay.jsx
+++ src/components/CurrentDealProductDisplay.jsx
@@ -11,7 +11,7 @@ class CurrentDealProductDisplay extends React.Component {
         let avg_diff_big = null;
         let disp_date = null;
         if (diff) {
-            disp_date = this.props.product.date.replace('+08:00', '').replace('T', ' ');
+            disp_date = this.props.product.date.substring(11, 19);
             lot_diff = diff.lot_diff;
             count_diff = diff.count_diff;
             avg_diff = diff.avg_diff.toFixed(2);
@@ -19,15 +19,19 @@ class CurrentDealProductDisplay extends React.Component {
             count_diff_big = diff.count_diff_big;
             avg_diff_big = diff.avg_diff_big.toFixed(2);
         }
-        const colorBackground = (diff) => {
+        const colorRedOrGreen = (value) => {
             const LargerThanZero = {
                 color: 'red',
             };
             const LesserThanZero = {
                 color: 'green',
             };
-            return diff>=0 ? LargerThanZero : LesserThanZero;
+            return value >= 0 ? LargerThanZero : LesserThanZero;
         };
+        const colorBlue = {
+            color: 'blue'
+        };
+
         return (
             <div>
                 <table>
@@ -37,36 +41,36 @@ class CurrentDealProductDisplay extends React.Component {
                         <td> {this.props.productName} </td>
                     </tr>
                     <tr>
-                        <td> 時間</td>
+                        <td>時間</td>
                         <td> {disp_date} </td>
                     </tr>
                     <tr>
-                        <td> 成交價</td>
-                        <td> {this.props.product.price} </td>
+                        <td>成交價</td>
+                        <td style={colorBlue}> {this.props.product.price} </td>
                     </tr>
                     <tr>
-                        <td> 口差</td>
-                        <td style={colorBackground(lot_diff)}> {lot_diff} </td>
+                        <td>口差</td>
+                        <td style={colorRedOrGreen(lot_diff)}> {lot_diff} </td>
                     </tr>
                     <tr>
-                        <td> 筆差</td>
-                        <td style={colorBackground(count_diff)}> {count_diff} </td>
+                        <td>筆差</td>
+                        <td style={colorRedOrGreen(count_diff)}> {count_diff} </td>
                     </tr>
                     <tr>
-                        <td> 均差</td>
-                        <td style={colorBackground(avg_diff)}> {avg_diff} </td>
+                        <td>均差</td>
+                        <td style={colorRedOrGreen(avg_diff)}> {avg_diff} </td>
                     </tr>
                     <tr>
-                        <td> 口差(大單)</td>
-                        <td style={colorBackground(lot_diff_big)}> {lot_diff_big} </td>
+                        <td>口差(大單)</td>
+                        <td style={colorRedOrGreen(lot_diff_big)}> {lot_diff_big} </td>
                     </tr>
                     <tr>
-                        <td> 筆差(大單)</td>
-                        <td style={colorBackground(count_diff_big)}> {count_diff_big} </td>
+                        <td>筆差(大單)</td>
+                        <td style={colorRedOrGreen(count_diff_big)}> {count_diff_big} </td>
                     </tr>
                     <tr>
-                        <td> 均差(大單)</td>
-                        <td style={colorBackground(avg_diff_big)}> {avg_diff_big} </td>
+                        <td>均差(大單)</td>
+                        <td style={colorRedOrGreen(avg_diff_big)}> {avg_diff_big} </td>
                     </tr>
                     </tbody>
                 </table>
