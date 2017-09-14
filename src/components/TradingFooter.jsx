71bf54da48d79179b25d62464a5f4cf905afca9a
Cory Lee <cory8249@gmail.com>
2017-09-14 22:05:24 +0800

    ui update

--- src/components/TradingFooter.jsx
+++ src/components/TradingFooter.jsx
@@ -1,10 +1,34 @@
-var React = require('react');
+let React = require('react');
 
 class TradingFooter extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            time: 0
+        }
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.setState({
+                time: Date.now()
+
+            ),
+            60000
+        )
+        ;
+    }
+
     render() {
+        let t = 0;
+        if (this.state.time) {
+            t = this.state.time;
+        }
+        let url = "http://www.surprice3c.com:8000/static/stocks.html?" + t;
         return (
-                <object type="text/html" data="http://www.surprice3c.com:8000/static/stocks.html" width="100%" height="800">
-                </object>
+            <object type="text/html" data={url} width="600px"
+                    height="600px"
+                    style={{overflow: 'hidden', border: '2px ridge blue'}}>
+            </object>
         )
     }
 }
