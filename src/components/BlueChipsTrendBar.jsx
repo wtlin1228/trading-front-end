28b1a63e58617f84907c7b2c8031e5f8bccbdf9a
Cory Lee <cory8249@gmail.com>
2017-09-17 10:05:02 +0800

    Add Websocket demo

--- src/components/BlueChipsTrendBar.jsx
+++ src/components/BlueChipsTrendBar.jsx
@@ -4,31 +4,34 @@ let HorizontalBar = require("react-chartjs-2").HorizontalBar;
 class BlueChipsTrendBar extends React.Component {
     render() {
         const symbolTable = {2330: "台積電", 2317: "鴻海", 6505: "台塑化"}
-        const blueChipsTrend = {2330: {movement: 1700, buy: 1122, sell: 300, buyPrice: 212.5, sellPrice: 213},
-                                2317: {movement: 500, buy: 650, sell: 954, buyPrice: 115, sellPrice: 115.5},
-                                6505: {movement: -270, buy: 305, sell: 270, buyPrice: 104.5, sellPrice: 105}
-                               }
+        const blueChipsTrend = {
+            2330: {movement: 1700, buy: 1122, sell: 300, buyPrice: 212.5, sellPrice: 213},
+            2317: {movement: 500, buy: 650, sell: 954, buyPrice: 115, sellPrice: 115.5},
+            6505: {movement: -270, buy: 305, sell: 270, buyPrice: 104.5, sellPrice: 105}
+        }
         const barChartData = {
-            labels: Object.keys(blueChipsTrend).map( (key, val)=> symbolTable[key]),
+            labels: Object.keys(blueChipsTrend).map((key, val) => symbolTable[key]),
             datasets: [
-                { 
+                {
                     label: '買',
                     backgroundColor: 'rgba(255,0,0,0.2)',
-                    data: Object.keys(blueChipsTrend).map((key)=> -blueChipsTrend[key].buy)
-                }, 
+                    data: Object.keys(blueChipsTrend).map((key) => -blueChipsTrend[key].buy)
+                },
                 {
                     label: '賣',
                     backgroundColor: 'rgba(0,255,0,0.2)',
-                    data: Object.keys(blueChipsTrend).map((key)=> blueChipsTrend[key].sell)
-                }] 
+                    data: Object.keys(blueChipsTrend).map((key) => blueChipsTrend[key].sell)
+                }]
         }
         const options = {
-            responsive: true,
-            scales: {xAxes: [{stacked: true}],
-                     yAxes: [{stacked: true}] }
+            responsive: false,
+            scales: {
+                xAxes: [{stacked: true}],
+                yAxes: [{stacked: true}]
+            }
         }
         return (
-            <HorizontalBar data={barChartData} options={options} height="30" />
+            <HorizontalBar data={barChartData} options={options} height="100" width="300"/>
         )
     }
 }
--- src/components/IndexMain.jsx
+++ src/components/IndexMain.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
 import * as actionCreators from '../actions'
-import { connect } from 'react-redux';
-import { Link } from "react-router-dom"
-import { Button } from 'antd';
-import BlueChipsTrendBar from "../components/BlueChipsTrendBar";
-import OptionsMajorPositionBar from "../components/OptionsMajorPositionBar";
-
+import {connect} from 'react-redux'
+import {Link} from "react-router-dom"
+import {Button} from 'antd'
+import BlueChipsTrendBar from "../components/BlueChipsTrendBar"
+import OptionsMajorPositionBar from "../components/OptionsMajorPositionBar"
+import WsQuote from "../components/WsQuote"
 
 class IndexMain extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClickButton = this.handleClickButton.bind(this)
-  }
+    constructor(props) {
+        super(props);
+        this.handleClickButton = this.handleClickButton.bind(this)
+    }
 
-  handleClickButton() {
-    this.props.addTest('123321')
-  }
+    handleClickButton() {
+        this.props.addTest('123321')
+    }
 
-  render() {
-    return (
-      <div className="ant-layout-content" style={{ padding: '0 50px' }}>
-        <Link to="/test">link to test</Link>
-        <Link to="/trading">link to trading</Link>
-        <Button type="primary" onClick={() => this.handleClickButton()}>addTest</Button>
-        <h1>Index</h1>
-        <BlueChipsTrendBar />
-        <OptionsMajorPositionBar />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="ant-layout-content" style={{padding: '0 50px'}}>
+                <Link to="/test">link to test</Link>
+                <Link to="/trading">link to trading</Link>
+                <Button type="primary" onClick={() => this.handleClickButton()}>addTest</Button>
+                <h1>Index</h1>
+                <WsQuote/>
+                <BlueChipsTrendBar/>
+                <OptionsMajorPositionBar/>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = store => (
-  {
-    testReducer: store.testReducer
-  }
+    {
+        testReducer: store.testReducer
+    }
 );
 
 export default connect(mapStateToProps, actionCreators)(IndexMain)
