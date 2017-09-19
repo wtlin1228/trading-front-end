e76075160ccf889b9dd4297580cf51e20ddda11d
Cory Lee <cory8249@gmail.com>
2017-09-19 15:36:50 +0800

    Refactor naming

--- src/components/WsQuote.jsx
+++ src/components/WsQuote.jsx
@@ -6,24 +6,24 @@ class WsQuote extends React.Component {
         this.state = {recvText: "", msg: []};
         this.msgCounter = 0;
         this.displayMsgLength = 5;
-        this.ws = this.init_ws("ws://www.surprice3c.com:8000/prices");
+        this.ws = this.initWs("ws://www.surprice3c.com:8000/prices");
     }
 
-    init_ws(ws_api) {
+    initWs(ws_api) {
         let socket = new WebSocket(ws_api);
 
         socket.onopen = function () {
             console.log("prices ws.onopen()")
         };
-        socket.onmessage = this.ws_onmessage.bind(this);
+        socket.onmessage = this.wsOnMessage.bind(this);
         socket.onclose = function () {
             console.log("ws.onclose");
-            this.ws_reconnect();
+            this.wsReconnect();
         }.bind(this);
         return socket;
     }
 
-    ws_onmessage(event) {
+    wsOnMessage(event) {
         let msg = this.state.msg;
         let tp = {counter: this.msgCounter, data: event.data};
         if (msg.length < this.displayMsgLength) {
@@ -36,17 +36,17 @@ class WsQuote extends React.Component {
         this.setState({msg: msg});
     }
 
-    ws_reconnect() {
-        console.log("ws_reconnect");
+    wsReconnect() {
+        console.log("wsReconnect");
         this.ws.close();
-        this.ws = this.init_ws();
+        this.ws = this.initWs();
     }
 
     componentDidMount() {
     }
 
     render() {
-        let msgList = this.state.msg.map((m) => <div>{m.counter} {m.data}<br/></div>);
+        let msgList = this.state.msg.map((m) => <div key={m.id}>{m.counter} {m.data}<br/></div>);
         return (
             <div>
                 <p> ----- Realtime Quote ----- </p>
