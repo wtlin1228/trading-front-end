e8f73eedf6269353e985442319aa6f9da9fee0a4
behappycc <behappyccc@gmail.com>
2017-09-23 09:46:35 +0800

    Add index cover

--- src/components/IndexMain.jsx
+++ src/components/IndexMain.jsx
@@ -9,6 +9,31 @@ import WsQuoteAll from './WsQuoteAll'
 import WsQuote from './WsQuote'
 import Ticker from '../components/Ticker'
 
+const Background = "../../static/img/cover.jpg"
+// http://localhost:8888/static/img/cover.jpg
+const innerHeight = window.innerHeight
+
+const coverStyle = {
+  margin: "-10px",
+  width: "100%",
+  height : (innerHeight -64) + 'px',
+  border: "0px",
+  backgroundImage: "url(" + Background + ")",
+  display: "table",
+  position: "relative",
+  WebkitBackgroundSize: "cover",
+  MozBackgroundSize: "cover",
+  backgroundSize: "cover",
+  OBackgroundSize: "cover",
+}
+
+const textStyle = {
+  color:"white",
+  fontSize: "50px",
+  textAlign: "center",
+  padding: (innerHeight / 2) - 64
+}
+
 class IndexMain extends React.Component {
     constructor(props) {
         super(props);
@@ -24,17 +49,23 @@ class IndexMain extends React.Component {
             'TX410500I7', 'TX410550I7', 'TX410600I7', 'TX410500U7', 'TX410550U7', 'TX410600U7'];
         let wsQuoteList = traceQuoteSymbols.map((sym) => <WsQuote symbol={sym} key={sym}/>);
         return (
-            <div className="ant-layout-content" style={{padding: '0 50px'}}>
-                <Link to="/test">link to test</Link>
-                <Link to="/trading">link to trading</Link>
-                <Button type="primary" onClick={() => this.handleClickButton()}>addTest</Button>
-                <h1>Index</h1>
-                <table><tr>{wsQuoteList}</tr></table>
-                <WsQuoteAll/>
-                <BlueChipsTrendBar/>
-                <OptionsMajorPositionBar/>
-                <Ticker/>
+        <div>
+          <section style={coverStyle}>
+            <h1 style={textStyle}>Hi, trading ai</h1>
+          </section>
+          <div className="ant-layout-content" style={{padding: '0 50px'}}>
+            <Link to="/test">link to test</Link>
+            <Link to="/trading">link to trading</Link>
+            <Button type="primary" onClick={() => this.handleClickButton()}>addTest</Button>
+            <h1>Index</h1>
+            <table><tr>{wsQuoteList}</tr></table>
+            <WsQuoteAll/>
+            <BlueChipsTrendBar/>
+            <OptionsMajorPositionBar/>
+            <Ticker/>
             </div>
+        </div>
+            
         )
     }
 }
