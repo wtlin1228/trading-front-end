49a921f7a7116dcb80e9d749706e5e10dada1f3c
wtlin1228 <wtlin0711@gmail.com>
2017-09-29 18:34:38 +0800

    Add logo to header for mobile index

--- src/components/header/HeaderPopover.jsx
+++ src/components/header/HeaderPopover.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import {Link} from "react-router-dom"
 import {Col, Popover, Button} from 'antd';
 
 import HeaderMenuInline from './HeaderMenuInline'
+
+const logoStyle = {
+  'width': '120px',
+  'height': '31px',
+  'background': '#333',
+  'border-radius': '6px',
+  'margin': '16px 24px 16px 0',
+  'float': 'left',
+};
+
+const textStyle = {
+  'fontFamily': 'Helvetica Neue For Number", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "Helvetica Neue", Helvetica, Arial, sans-serif',
+  'textAlign': 'center',
+  'fontSize': '12px',
+  'color': 'rgba(255, 255, 255, 0.67)',
+  'margin': '-16px',
+};
+
+const hamburgerMenuStyle = {
+  'background': 'none',
+  'border': 'none',
+  'display': 'inline-block',
+  'position': 'fixed',
+  'right': '0px',
+  'top': '18px',
+  'zIndex': '1000',
+};
+
+const iconBarStyle = {
+  'backgroundColor': '#616161',
+  'display': 'block',
+  'height': '4px',
+  'margin': '0 auto 4px',
+  'width': '30px',
+};
 
 
 class HeaderPopover extends React.Component {
@@ -11,24 +47,6 @@ class HeaderPopover extends React.Component {
   }
 
   render() {
-    const hamburgerMenuStyle = {
-      'background': 'none',
-      'border': 'none',
-      'display': 'inline-block',
-      'position': 'fixed',
-      'right': '0px',
-      'top': '18px',
-      'zIndex': '1000',
-    };
-
-    const iconBarStyle = {
-      'backgroundColor': '#616161',
-      'display': 'block',
-      'height': '4px',
-      'margin': '0 auto 4px',
-      'width': '30px',
-    };
-
     const popover = [
       <Popover
         placement="bottomRight"
@@ -45,6 +63,13 @@ class HeaderPopover extends React.Component {
 
     return (
       <Col>
+        <div className="logo" style={logoStyle}>
+          <Link to='/'>
+            <p style={textStyle}>
+              Seafood AI
+            </p>
+          </Link>
+        </div>
         {popover}
       </Col>
     );
