7f45df057d43cf68649a0a5a26b916bd80942855
Cory Lee <cory8249@gmail.com>
2017-09-29 22:44:56 +0800

    Update sider name

--- src/components/TSider.jsx
+++ src/components/TSider.jsx
@@ -60,26 +60,33 @@ class TSider extends React.Component {
         </div>
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
           <Menu.Item key="1">
-            <Icon type="pie-chart"/>
-            <span>Option 1</span>
+            <Icon type="area-chart"/>
+            <span>買賣力差</span>
           </Menu.Item>
           <Menu.Item key="2">
-            <Icon type="desktop"/>
-            <span>Option 2</span>
+            <Icon type="pie-chart"/>
+            <span>權值股動向</span>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Icon type="dot-chart"/>
+            <span>籌碼分析</span>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <Icon type="bar-chart"/>
+            <span>選擇權分析</span>
           </Menu.Item>
           <SubMenu
             key="sub1"
             title={<span><Icon type="user"/><span>User</span></span>}
           >
-            <Menu.Item key="3">Tom</Menu.Item>
-            <Menu.Item key="4">Bill</Menu.Item>
-            <Menu.Item key="5">Alex</Menu.Item>
+            <Menu.Item key="5">Bill</Menu.Item>
+            <Menu.Item key="6">Alex</Menu.Item>
           </SubMenu>
           <SubMenu
             key="sub2"
             title={<span><Icon type="team"/><span>Team</span></span>}
           >
-            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="7">Team 1</Menu.Item>
             <Menu.Item key="8">Team 2</Menu.Item>
           </SubMenu>
           <Menu.Item key="9">
