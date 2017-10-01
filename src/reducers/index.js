7162fef6f5f99ea724f42a4ce5b436d45ad18473
wtlin1228 <wtlin0711@gmail.com>
2017-10-02 07:48:12 +0800

    combine productsReducer into rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import testReducer from './testReducer'
 import collapsedReducer from './collapsedReducer'
+import productsReducer from './productsReducer'
 
 const rootReducer = combineReducers({
   testReducer: testReducer,
-  collapsedReducer: collapsedReducer
+  collapsedReducer: collapsedReducer,
+  productsReducer: productsReducer
 })
 
 export default rootReducer
